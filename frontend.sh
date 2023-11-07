2ed28d4f299a824557e9d639742ba5174d209ce1
dbavenkatesh18 <dbavenkatesh18@gmail.com>
2023-11-07 13:00:51 +0530

    aws-devops

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
 echo -e "\e[36m Installing nginx \e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>/tmp/expense.log
 
 echo -e "\e[36m Copying expense config file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 
 echo -e "\e[36m Clean old nginx content \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
 echo -e "\e[36m Download front application code \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
 echo -e "\e[36m Extract downloaded application content \e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 
 echo -e "\e[36m Starting nginx service \e[0m"
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>>/tmp/expense.log
+systemctl start nginx &>>/tmp/expense.log
